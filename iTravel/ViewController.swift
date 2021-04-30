f6dd615d8ae0b23163d2e87cb8b014888b4f0938
shao1-crypto <77039804+shao1-crypto@users.noreply.github.com>
2021-04-29 22:18:21 -0700

    Initial Commit

---
+++ iTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTravel
+//
+//  Created by Shijia Hao on 4/29/21.
+//  Copyright © 2021 shijiah2012@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
